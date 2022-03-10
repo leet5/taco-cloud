a9632a1067298e6a094bd8b65e2061cc59e2097f
leet5 <leet5@protonmail.com>
2022-03-10 18:50:43 +0300

    HomeController test fix

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -3,7 +3,12 @@ package tacos;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import tacos.repositories.IngredientRepository;
+import tacos.repositories.OrderRepository;
+import tacos.repositories.UserRepository;
+import tacos.services.OrderAdminService;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -11,6 +16,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest
 public class HomeControllerTest {
+
+    @MockBean
+    private OrderRepository orderRepository;
+
+    @MockBean
+    private IngredientRepository ingredientRepository;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private OrderAdminService adminService;
 
     @Autowired
     private MockMvc mockMvc;
