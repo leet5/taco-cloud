eb3bca4e71963525f4ebd6967a2c51a60b3b06e8
leet5 <leet5@protonmail.com>
2022-03-12 23:09:03 +0300

    PostgreSQL as main RDBMS (docker-upd)

--- src/main/java/tacos/configurations/WebConfig.java
+++ src/main/java/tacos/configurations/WebConfig.java
@@ -3,10 +3,14 @@ package tacos.configurations;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import tacos.domain.Address;
 import tacos.domain.Ingredient;
+import tacos.domain.User;
 import tacos.repositories.IngredientRepository;
+import tacos.repositories.UserRepository;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -17,18 +21,21 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Bean
-    public CommandLineRunner dataLoader(IngredientRepository repo) {
+    public CommandLineRunner dataLoader(IngredientRepository ingredientRepo, UserRepository userRepo, PasswordEncoder encoder) {
         return args -> {
-            repo.save(new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP));
-            repo.save(new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP));
-            repo.save(new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN));
-            repo.save(new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN));
-            repo.save(new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES));
-            repo.save(new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES));
-            repo.save(new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE));
-            repo.save(new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE));
-            repo.save(new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE));
-            repo.save(new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE));
+            ingredientRepo.save(new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP));
+            ingredientRepo.save(new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP));
+            ingredientRepo.save(new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN));
+            ingredientRepo.save(new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN));
+            ingredientRepo.save(new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES));
+            ingredientRepo.save(new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES));
+            ingredientRepo.save(new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE));
+            ingredientRepo.save(new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE));
+            ingredientRepo.save(new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE));
+            ingredientRepo.save(new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE));
+
+            final Address address = new Address("Home", "Street", "City", "MW", "600000");
+            userRepo.save(new User("admin", "admin", "admin", "+79999999999", address));
         };
     }
 }
